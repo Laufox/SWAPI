fb821e9b265c9de5f18559545fe04cf61c5cce1d
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-04-21 10:09:10 +0200

    Initial commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navigation = () => {
+	return (
+		<div>Navigation</div>
+	)
+}
+
+export default Navigation
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const Homepage = () => {
+  return (
+    <>
+    <h1>Welcome to star wars</h1>
+    </>
+  )
+}
+
+export default Homepage
