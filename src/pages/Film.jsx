81aecfe23f5b0faf6ce4dba84b206c511977201c
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-04-27 20:14:00 +0200

    Styling individual film page

--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -1,11 +1,13 @@
 // Import hooks from react
 import { useEffect, useState } from 'react';
 // Import from react-router-dom
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 // Import bootstrap components
 import Button from 'react-bootstrap/Button';
 // Import functions that communicate with SWAPI API
 import SwapiAPI from '../services/SwapiAPI';
+// Import function to extract ID value from URL
+import { getIdFromUrl } from '../services/getIdFromUrl'
 
 /**
  * 
@@ -39,7 +41,34 @@ const Film = () => {
             <h1>Movie info</h1>
 
             {
-                filmData && <h2>{filmData.title}</h2>
+                filmData && (
+                    <article className='film-item-article'>
+                        <header>
+                            <h2>{filmData.title}</h2>
+                        </header>
+
+                        <main>
+                            <h3>About</h3>
+                            <pre>{filmData.opening_crawl}</pre>
+                            <aside>
+                                <p>Episode: { filmData.episode_id}</p>
+                                <p>Released: { filmData.release_date }</p>
+                                <p>Directed by: { filmData.director }</p>
+                            </aside>      
+                        </main>
+
+                        <footer>
+                            <h3>Characters featured: </h3>
+                            <div className='character-list'>
+                            {
+                                filmData.characters.map( (character, index) => {
+                                    return <Button key={index} variant="info" as={Link} to={`/people/${getIdFromUrl(character)}`}>{getIdFromUrl(character).padStart(2, '0')}</Button>
+                                } )
+                            }
+                            </div>
+                        </footer>
+                    </article>
+                )
             }
         </>
     )
