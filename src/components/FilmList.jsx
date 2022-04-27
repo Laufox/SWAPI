9ec4d377588b7264328a8621edd133af6962fa76
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-04-27 19:04:14 +0200

    Styling filmlist component

--- src/components/FilmList.jsx
+++ src/components/FilmList.jsx
@@ -4,6 +4,7 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import { Link } from "react-router-dom"
 // Import function to extract ID value from URL
 import { getIdFromUrl } from '../services/getIdFromUrl'
+import Button from 'react-bootstrap/Button';
 
 /** 
  *  Component to display a list of films from SWAPI API
@@ -14,12 +15,28 @@ const FilmList = ({list, title}) => {
         <section>
             <h2>{title}</h2>
 
-            <ListGroup>
+            <ListGroup className='swapi-list'>
                 {
                     // Loop over all items in list as a clickable link-item
                     list.map( (item, index) => {
-                        return <ListGroup.Item key={index} action as={Link} to={`/films/${getIdFromUrl(item.url)}`}>
-                                    <h3>{item.title}</h3>
+                        return <ListGroup.Item key={index} action as={Link} to={`/films/${getIdFromUrl(item.url)}`} className='swapi-list-item'>
+
+                                    <article>
+
+                                        <header>
+                                            <h3>{item.title}</h3>
+                                        </header>
+
+                                        <main>
+                                            { item.release_date } / { item.director } / { item.characters.length } characters
+                                        </main>
+
+                                        <footer>
+                                            <Button variant="info">More info about {item.title}</Button>
+                                        </footer>
+
+                                    </article>
+                                    
                                </ListGroup.Item>
                     } )
                 }
