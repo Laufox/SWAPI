f61bfb1fdec88819d80505f5c699af9e3d410019
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-04-28 08:02:51 +0200

    Ads direct links to people and film from index page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,20 @@
-/**
- *  Page for traffic to /
- */
+import { Link } from "react-router-dom"
+
 const Homepage = () => {
     return (
         <>
             <h1>Welcome to star wars</h1>
+
+            <ul className="index-list">
+                <li>
+                    <Link to="/people" >View character list</Link>
+                </li>
+                <li>
+                    <Link to="/films" >View movie list</Link>
+                </li>
+            </ul>
+            
+            
         </>
     )
 }
